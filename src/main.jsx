62ef5e0c81f7627f333de3f6e51a7bcc899733b6
LidorPrototype <ivanlidor98@gmail.com>
2025-06-16 18:03:16 +0300

    env related banner

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,41 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const environment = import.meta.env.VITE_ENVIRONMENT?.trim() || "unknown";
+let color;
+/*
+ * #16a34a
+ * #dc2626
+ * #eab308
+**/
+if (environment === "Pre-Prod") color = "#16a34a";
+else if (environment === "Production") color = "#dc2626";
+else if (environment === "unknown") color = "#eab308";
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {environment && (
+      <div
+        style={{
+          position: 'fixed',
+          top: 0,
+          right: 0,
+          zIndex: 9999,
+          backgroundColor: color,
+          color: '#fff',
+          fontSize: '0.75rem',
+          fontWeight: '700',
+          fontFamily: 'sans-serif',
+          padding: '0.25rem 2.5rem',
+          transform: 'rotate(45deg) translate(35%, -70%)',
+          boxShadow: '0 0 10px rgba(0,0,0,0.3)',
+          pointerEvents: 'none',
+          textTransform: 'uppercase',
+        }}
+      >
+        {environment.toUpperCase()}
+      </div>
+    )}
     <App />
   </StrictMode>,
 )
